23116851845ad9b95de753fd94de87f1447f24c9
jhwong18 <56313884+jhwong18@users.noreply.github.com>
2019-11-10 12:11:13 +0800

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+echo "This script prints out the Exploratory Data Analysis + Traffic Volume prediction python notebooks"
+python3 eda.ipynb
+echo "Exploratory Data Analysis notebook has been red"
+python3 traffic_prediction.ipynb
+echo "Traffic Prediction notebook has been red"
